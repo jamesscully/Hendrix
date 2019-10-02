c2b63b053801ec311fe5a239a6541393eb286cfe
James Scully <james.will.scully@gmail.com>
2019-10-02 01:04:17 +0100

    ID3v1 Genre tags are now able to be processed

--- app/src/main/java/com/scullyapps/hendrix/data/song/Song.kt
+++ app/src/main/java/com/scullyapps/hendrix/data/song/Song.kt
@@ -1,22 +1,69 @@
 package com.scullyapps.hendrix.data.song
 
+import android.media.MediaMetadataRetriever
+import android.util.Log
+import com.scullyapps.hendrix.data.ID3GenreResolver
 import java.io.File
+import java.lang.IllegalStateException
 
 class Song (file : File) {
     private val TAG: String = "Song"
 
+
+
+    var title  : String = "Unknown Track"
+    var artist : String = "Unknown Artist"
+    var genre  : String = "Unknown Genre"
+    var duration : String = "Unknown Length"
+
+
+
+
+
+
     init {
-        require(isValidAudio(file.extension))
+        require(isValidAudioExt(file.extension))
+
+        val mmr = MediaMetadataRetriever()
+
+        mmr.setDataSource(file.path)
+
+        //Log.d(TAG, "Editing file ${file.name} path: ${file.path}")
+
+        try {
+            title = mmr.extractMetadata(MediaMetadataRetriever.METADATA_KEY_TITLE)
+            artist = mmr.extractMetadata(MediaMetadataRetriever.METADATA_KEY_ARTIST)
+            duration = mmr.extractMetadata(MediaMetadataRetriever.METADATA_KEY_DURATION)
+            genre = mmr.extractMetadata(MediaMetadataRetriever.METADATA_KEY_GENRE)
+
+            var te : String = ""
+
+            // if no freetext genre is found, then it will be returned as, i.e. (00) for blues
+            if(genre.matches( Regex("^\\([0-9]*\\)\$") )) {
+                genre = ID3GenreResolver.codeToString(genre.substring(1, genre.length - 1).toInt())
+            }
+
+        } catch (e: IllegalStateException) {
+
+        }
+
+
+
     }
 
+    override fun toString(): String {
 
+        // The Weeknd - Starboy 03:15 HipHop
 
+        return "$artist - $title $duration $genre $"
+    }
 
+    companion object {
+        fun isValidAudioExt(fileExt: String): Boolean {
+            val acceptedFileTypes: Array<String> = arrayOf("mp3", "m4a")
 
-    fun isValidAudio(fileExt : String) : Boolean {
-        val acceptedFileTypes : Array<String> = arrayOf("mp3", "m4a")
-
-        return acceptedFileTypes.contains(fileExt)
+            return acceptedFileTypes.contains(fileExt)
+        }
     }
 
 }
--- app/src/main/java/com/scullyapps/hendrix/services/DiscoverMusic.kt
+++ app/src/main/java/com/scullyapps/hendrix/services/DiscoverMusic.kt
@@ -2,21 +2,16 @@ package com.scullyapps.hendrix.services
 
 import android.Manifest
 import android.app.IntentService
-import android.app.Service
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.media.MediaMetadataRetriever
 import android.os.Environment
-import android.os.IBinder
 import android.util.Log
 import androidx.core.content.ContextCompat
 import com.scullyapps.hendrix.data.song.Song
 import java.io.File
-import java.sql.Time
-import java.util.*
-import java.util.concurrent.TimeUnit
-import kotlin.system.measureNanoTime
 import kotlin.system.measureTimeMillis
+
 
 class DiscoverMusic : IntentService("DiscoverMusicService") {
 
@@ -37,15 +32,12 @@ class DiscoverMusic : IntentService("DiscoverMusicService") {
             val timeExec = measureTimeMillis {
 
                 File(Environment.getExternalStorageDirectory(), "Music").walkTopDown().forEach {
-                    val mmr = MediaMetadataRetriever()
 
-                    if (it.isFile && it.extension == "mp3") {
-                        mmr.setDataSource(it.absolutePath)
+                    // ensure we're only reading valid audio files
+                    if (it.isFile && Song.isValidAudioExt(it.extension) ) {
+                        val newSong = Song(it)
 
-                        var artistName =
-                            mmr.extractMetadata(MediaMetadataRetriever.METADATA_KEY_ARTIST)
-                        var songName =
-                            mmr.extractMetadata(MediaMetadataRetriever.METADATA_KEY_TITLE)
+                        println(newSong)
 
                     }
                 }
